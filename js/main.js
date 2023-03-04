e3fa3e2823581064c298f14e3f98be7b26f7a815
Sotiris Ntokas <ntokas1995@live.com>
2023-03-04 14:16:41 +0100

    altered my view a little bit for my better understunding

--- js/main.js
+++ js/main.js
@@ -1,21 +1,55 @@
 class Game {
   constructor() {
-    this.player = null;
-    this.keeper = null;
-    this.ball = null;
+ 
   }
    start(){
-    this.ball = new Ball ();
+    const keeperElm = document.getElementById("keeper");
+
+
+  }
+  jump();
+   
 
     this.attachEventListeners();
    }
+   start(){
+    this.player = new Player();
+    this.attachEventListeners();
 
+   }
 
   attachEventListeners(){
     document.addEventListener("keydown", (e) => {
       if (e.key === "Space"){
         this.ball.moveRight();
-      }
+        }
     });
   }
 }
+
+
+class Player {
+  constructor() {
+
+  }
+}
+
+class Keeper {
+  constructor (){
+
+    }
+    function jump(){
+      setInterval (() => {
+        keeperElm.classList.toggle("jump");
+      }, 500);
+    }
+}
+
+class Ball {
+  constructor(){
+
+  }
+}
+
+
+document.addEventListener
