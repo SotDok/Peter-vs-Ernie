5bd8cc05ec0a1a1c6feddbf6abc5bc0177a02a73
Sotiris Ntokas <ntokas1995@live.com>
2023-03-07 18:42:25 +0100

    made the bullets move and hide when they collide with the enemy. Also added some css

--- js/main.js
+++ js/main.js
@@ -1,43 +1,95 @@
+
+
+
 class Game {
   constructor() {
     this.player = null;
     this.enemiesArr = [];
+    this.bulletsArr = [];
   }
   play() {
     this.player = new Player();
-    
+
     this.attachEventListeners();
 
-    setInterval( () => {
+    setInterval(() => {
       const myEnemy = new Enemy();
       this.enemiesArr.push(myEnemy);
     }, 3000);
 
-    setInterval( () => {
+    setInterval(() => {
       this.enemiesArr.forEach((enemyInstance) => {
         enemyInstance.moveLeft();
-
-        if (
-          this.player.positionY + this.player.height >= enemyInstance.positionY &&
-          this.player.positionY <= enemyInstance.positionY + enemyInstance.height &&
-          this.player.positionX + this.player.width >= enemyInstance.positionX &&
-          this.player.positionX <= enemyInstance.positionX + enemyInstance.width
-        ) {
-          console.log("game over");
-        }
-        
+        this.detectCollision(enemyInstance);
+        this.removeEnemy(enemyInstance);
       });
-    }, 20);
+
+      this.bulletsArr.forEach((bulletInstance) => {
+        bulletInstance.moveRight();
+        this.detectBulletCollision(bulletInstance);
+        this.removeBullet(bulletInstance);
+      });
+    }, 50);
   }
-  attachEventListeners(){
+  attachEventListeners() {
     document.addEventListener("keydown", (e) => {
       if (e.key === "ArrowUp") {
+        
         this.player.moveUp();
       } else if (e.key === "ArrowDown") {
+        
         this.player.moveDown();
+      } else if (e.key === " ") {
+        
+        const myBullet = new Bullet (this.player.positionX, this.player.positionY);
+        this.bulletsArr.push(myBullet);
       }
     });
+  }
+  detectCollision(enemyInstance) {
+    if (
+      this.player.positionY + this.player.height >= enemyInstance.positionY &&
+      this.player.positionY <= enemyInstance.positionY + enemyInstance.height &&
+      this.player.positionX + this.player.width >= enemyInstance.positionX &&
+      this.player.positionX <= enemyInstance.positionX + enemyInstance.width
+    ) {
     
+  
+       
+    }
+  }
+  detectBulletCollision(bulletInstance) {
+    this.enemiesArr.forEach((enemyInstance) => {
+      if (
+        bulletInstance.positionY >= enemyInstance.positionY &&
+        bulletInstance.positionY <= enemyInstance.positionY + enemyInstance.height &&
+        bulletInstance.positionX + bulletInstance.width >= enemyInstance.positionX &&
+        bulletInstance.positionX <= enemyInstance.positionX + enemyInstance.width
+      ) {
+        
+        this.removeEnemy(enemyInstance);
+        enemyInstance.hide();
+        this.removeBullet(bulletInstance);
+        bulletInstance.hide();
+        
+      }
+    });
+  }
+
+  removeEnemy(enemyInstance) {
+    if (enemyInstance.positionX <= -enemyInstance.width) {
+      const enemyIndex = this.enemiesArr.indexOf(enemyInstance);
+      this.enemiesArr.splice(enemyIndex, 1);
+      enemyInstance.enemyElm.remove();
+    }
+  }
+  removeBullet(bulletInstance) {
+    const boardElm = document.getElementById("board");
+    if (bulletInstance.positionX >= 90) {
+      const bulletIndex = this.bulletsArr.indexOf(bulletInstance);
+      this.bulletsArr.splice(bulletIndex, 1);
+      bulletInstance.removeBullet();
+    }
   }
 }
 
@@ -53,12 +105,51 @@ class Player {
     this.playerElm.style.height = this.height + "vh";
   }
   moveUp() {
-    this.positionY--;
-    this.playerElm.style.top = this.positionY + "vh";
+    if (this.positionY > 0) {
+      this.positionY = this.positionY - 3;
+      this.playerElm.style.top = this.positionY + "vh";
+    }
   }
   moveDown() {
-    this.positionY++;
-    this.playerElm.style.top = this.positionY + "vh";
+    if (this.positionY < 100 - this.height) {
+      this.positionY = this.positionY + 3;
+      this.playerElm.style.top = this.positionY + "vh";
+    }
+  }
+}
+
+class Bullet {
+  constructor(playerPositionX, playerPositionY) {
+    this.width = 3;
+    this.height = 3;
+    this.positionX = playerPositionX + 10;
+    this.positionY = playerPositionY + 2;
+    this.bulletElm = document.createElement("div");
+
+    this.createDomElement();
+  }
+
+  createDomElement() {
+    this.bulletElm.className = "bullet";
+    this.bulletElm.style.width = this.width + "vw";
+    this.bulletElm.style.height = this.height + "vh";
+    this.bulletElm.style.top = this.positionY + "vh";
+    this.bulletElm.style.left = this.positionX + "vw";
+
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.bulletElm);
+  }
+
+  moveRight() {
+    this.positionX = this.positionX + 3;
+    this.bulletElm.style.left = this.positionX + "vw";
+  }
+
+  removeBullet() {
+    this.bulletElm.remove();
+  }
+  hide(){
+    this.bulletElm.style.display = "none";
   }
 }
 
@@ -67,7 +158,7 @@ class Enemy {
     this.width = 10;
     this.height = 20;
     this.positionX = 100;
-    this.positionY = Math.floor(Math.random() * 70) + 10;
+    this.positionY = Math.floor(Math.random() * 80);
     this.enemyElm = null;
 
     this.createDomElement();
@@ -88,11 +179,10 @@ class Enemy {
     this.positionX--;
     this.enemyElm.style.left = this.positionX + "vw";
   }
+  hide(){
+    this.enemyElm.style.display = "none";
+  }
 }
 
 const game = new Game();
 game.play();
-
-
-
-
